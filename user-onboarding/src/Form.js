06220d92302524d08a9dcdafb5df3c8a13b99964
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-19 14:03:22 -0800

    added validation schema

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -3,7 +3,11 @@ import * as Yup from "yup";
 import axios from "axios";
 import { withFormik, Form, Field } from "formik";
 
-const PersonForm = ({ }) => {
+// 1. Create Formik Form. 
+// Used <Form>, label htmoFor, then Field along with id, type, name, placeholder to create initial boxes
+// Made button with <button> and type:checkmark to create Terms of Service box.
+
+const PersonForm = ({}) => {
     return (
 
         <div className='person-form'>
@@ -26,11 +30,10 @@ const PersonForm = ({ }) => {
                   <label htmlFor='Password'>Password:</label>
                 <Field
                     id="password"
-                    type="text"
+                    type="password"
                     name="userPassword"
                     placeholder="Password"
     
-            
                 />
                 <br></br>
                 <label htmlFor="terms" className="checkbox-container">Terms of Service</label>
@@ -39,18 +42,49 @@ const PersonForm = ({ }) => {
                  type="checkbox"
                  name="terms"
                  />
-                    
-
-                
-                <br></br>
+                 <br></br>
                 <button type="submit">Submit</button>
 
             </Form>
         </div>
+     
 
-    )
-}
+    );
+};
 
-const FormikPersonForm = withFormik({})(PersonForm)
+
+
+const FormikPersonForm = withFormik({
+    mapPropsToValues ({userName, userEmail, userPassword,terms }){
+        return{
+            userName: userName || '',
+            userEmail: userEmail || '',
+            userPassword: userPassword || '',
+            terms: terms || false
+
+        };
+    },
+//Validation schema
+        validationSchema: Yup.object().shape({
+            userName: Yup.string().required(
+                "Please put in a name"
+            ),
+            userEmail: Yup.string().required(
+                "Please put in an email"
+            ),
+
+            userPassword: Yup.string().required(
+                "Please put in a password"
+            ),
+
+
+            terms: Yup.boolean().oneOf([true], "Must Accept Terms and Conditions")
+
+        })
+
+//end of validation schema
+
+    
+})(PersonForm)
 
 export default FormikPersonForm
