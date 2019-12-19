7b0b54b66f52d13fdb2792a4790ee061962b1e40
doren <56702391+doranium2013@users.noreply.github.com>
2019-12-19 13:28:34 -0800

    added terms of service checkbox and submit button

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -29,7 +29,21 @@ const PersonForm = ({ }) => {
                     type="text"
                     name="userPassword"
                     placeholder="Password"
+    
+            
                 />
+                <br></br>
+                <label htmlFor="terms" className="checkbox-container">Terms of Service</label>
+                <Field
+                 id="terms"
+                 type="checkbox"
+                 name="terms"
+                 />
+                    
+
+                
+                <br></br>
+                <button type="submit">Submit</button>
 
             </Form>
         </div>
